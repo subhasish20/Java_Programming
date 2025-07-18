28d9020862cf3eaf693ecfd200e9d5f7cef8e065
subhasish20 <subhasishjena8280@gmail.com>
2025-07-18 22:54:05 +0530

    na

---
+++ assignment.java
@@ -0,0 +1,8 @@
+public class assignment {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("before using the assignment operator the value of a is :"+a);
+         a+=6;
+        System.out.println("after usig the assignment operator the value of a is :"+a);
+    }
+}
