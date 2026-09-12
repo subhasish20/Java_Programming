c4b1de460ac5b155c673dc4dd2409bb0bb8ae73d
Subhasish Jena <subhasishjena8280@gmail.com>
2026-09-13 00:29:28 +0530

    code

---
+++ Intro/hello_word.java
@@ -0,0 +1,6 @@
+
+public class hello_word {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+};
---
+++ Intro/printUsingPrintf.java
@@ -0,0 +1,6 @@
+public class printUsingPrintf {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello !! I'm Subhasish Jena");
+    }
+}
